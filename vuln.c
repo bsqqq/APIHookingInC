391c75127605510944fad0baf5dc03472188e23e
Vinicius Aragão <55242418+bsqqq@users.noreply.github.com>
2022-09-09 14:57:07 -0300

    Update file to follow in commentary

--- vuln.c
+++ vuln.c
@@ -4,7 +4,7 @@
 // Aqui devemos dar um jeito de chegar na string da funcao vitoria() 
 // porem sem chamar a funcao no corpo da main, apenas usando tecnicas de Buffer Overflow.
 // recomendo usar o x32dbg para verificar quantos bytes sao necessarios para sobescrever o endereco de retorno da funcao... 😁
-// para mais detalhes, consulte o arquivo exploit.c
+// para mais detalhes, consulte o arquivo exploit.py
 
 void vitoria() {
     printf("Parabens, vc conseguiu!");
